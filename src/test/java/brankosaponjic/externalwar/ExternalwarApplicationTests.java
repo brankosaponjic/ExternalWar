fef79fbe46d31c43425ef6c2fc43dd1e60a8aceb
Branko <brankosaponjic76@gmail.com>
2021-03-18 12:47:29 +0100

    Initial commit

---
+++ src/test/java/brankosaponjic/externalwar/ExternalwarApplicationTests.java
@@ -0,0 +1,13 @@
+package brankosaponjic.externalwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalwarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
